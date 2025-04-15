420886c77a8eb509c52bddf24d796df0093ec085
sujataprasad01 <sujataprasad2004@gmail.com>
2025-04-15 20:38:24 +0530

    database connected

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,12 @@
 import express from 'express'
 import cors from 'cors'
 import 'dotenv/config'
-
+import connectDB from './config/mongodb.js'
 // app
 
 const app=express()
 const port=process.env.PORT || 4000
+connectDB()
 
 // middlewares
 
