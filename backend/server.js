2502a6ecc88daf5684238462d2a30c0cf3d96ed8
sujataprasad01 <sujataprasad2004@gmail.com>
2025-04-17 16:00:51 +0530

    now admin can add doctors

--- backend/server.js
+++ backend/server.js
@@ -2,11 +2,14 @@ import express from 'express'
 import cors from 'cors'
 import 'dotenv/config'
 import connectDB from './config/mongodb.js'
+import connectCloudinary from './config/cloudinary.js'
+import adminRouter from './routes/adminRoute.js'
 // app
 
 const app=express()
 const port=process.env.PORT || 4000
 connectDB()
+connectCloudinary()
 
 // middlewares
 
@@ -14,7 +17,8 @@ app.use(express.json())
 app.use(cors())
 
 // api endpoints
-
+app.use('/api/admin', adminRouter)
+// localhost:4000/api/admin/add-doctor
 app.get('/', (req, res)=>{
  res.send("Api working")
 })
