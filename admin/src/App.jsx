9a1dcf16de452ef9ca1ef407de85a35fb0482ff1
sujataprasad01 <sujataprasad2004@gmail.com>
2025-04-18 00:46:40 +0530

    created admin navbar

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -3,11 +3,14 @@ import Login from './pages/Login'
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css'
 import { AdminContext } from './context/AdminContext';
+import Navbar from './components/Navbar';
 const App = () => {
   const {aToken}=useContext(AdminContext)
+
   return aToken ?(
     <div>
       <ToastContainer></ToastContainer>
+      <Navbar></Navbar>
     </div>
   ):(
     <>
---
+++ admin/src/components/Navbar.jsx
@@ -0,0 +1,52 @@
+import React, { useState } from 'react'
+import logo from '../assets/logo.png'
+import { useLocation,  Link, useNavigate } from 'react-router-dom'
+import profileImg from '../assets/profile.png'
+import { useContext } from 'react'
+import { AdminContext } from '../context/AdminContext'
+const Navabr = () => {
+    const navigate=useNavigate();
+    const location = useLocation();
+    // const [showMenu, setShowMenu]=useState(false)
+    const[token, setToken]=useState(true)
+    const [logout, setLogout]=useState(false)
+    const {aToken, setAToken}=useContext(AdminContext)
+    const handleLogout = () => {
+        aToken && setAToken('')
+            aToken && localStorage.removeItem('aToken')
+        setLogout(true);
+        setToken(false);
+        navigate('/')
+      };
+  return (
+  <>
+   <div className='container flex flex-row justify-between pl-16 pr-16'>
+      <div className='flex flex-row m-5'>
+        <img src={logo} className='w-16 h-16 drop-shadow-[3px_3px_0px_rgba(0,0,0,0.2)]'></img>
+        <Link to='/'>
+      <h2 className='text-2xl mt-7 ml-2 font-semibold drop-shadow-[3px_3px_0px_rgba(0,0,0,0.2)]'><span className="text-customPurple">CARE</span> Connect</h2>
+      </Link>
+      <p className='rounded-full text-sm font-medium border-2 h-10 p-2 mt-5 ml-5'>{aToken?'Admin':'Doctor'}</p>
+      </div>
+     
+      <div className='m-5 flex'>
+      <Link to='/about' className='text-customPurple rounded-md drop-shadow-[3px_3px_0px_rgba(0,0,0,0.2)] m-7 text-lg'>About</Link>
+
+      {
+    token ? (
+        location.pathname === '/profile' ? (
+            <div onClick={handleLogout} className='text-customPurple rounded-md drop-shadow-[3px_3px_0px_rgba(0,0,0,0.2)] m-7 text-lg'><Link to='/login'>Logout</Link></div>
+        ) : (
+            <div><Link to='/profile'><img src={profileImg} className='w-16 h-16 p-2 mt-2 ml-4'></img></Link></div>)
+    ) : (
+        <button onClick={() => navigate('/login')}
+            className='text-customPurple rounded-md drop-shadow-[3px_3px_0px_rgba(0,0,0,0.2)] m-7 text-lg'>Login</button>
+    )
+}
+      </div>
+    </div>
+  </>
+  )
+}
+
+export default Navabr
